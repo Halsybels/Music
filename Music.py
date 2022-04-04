a170e1d112344e52d6a20ae401cdb45adc6620e7
Halsybels <98822428+Halsybels@users.noreply.github.com>
2022-04-04 12:24:12 +0100

    Add files via upload

--- Music.py
+++ Music.py
@@ -1,10 +1,11 @@
 def menu(login,register):
-    menu = input("Welcome to OCR's greatest music app. Choose either to register by pressing 1 else type anything else:")
-    if menu == "1":
+    menu = input("Welcome to OCR's greatest music app. To register press 'r'. To login press 'l':")
+    if menu == "r":
         register()
-    else:
-        login()
-
+    elif menu == "l":
+        login(playlists,add_songs,view_songs)
+    
+    
         
 def register():
     username = input("Please create a username:")
@@ -16,8 +17,9 @@ def register():
     writefile = open("Account.txt","w")
     writefile.write(username + '\n' + password + '\n' + name + '\n' + dob + '\n' + fave_artist + '\n' + fave_genre + '\n')
     writefile.close()
+    menu(login,register)  
     
-def login():
+def login(playlists,add_songs,view_songs):
     checkusername = input("Please enter existing username:")
     checkpassword = input("Please enter existing password:")
     file = open('Account.txt' , 'r')
@@ -28,27 +30,53 @@ def login():
     else:
         print("Access denied! Check Username and Password.")
         file.close()
-    song_list = input("Would you like to see your song titles?")
-    if song_list == 'yes':
-        with open('Songs.txt', 'r') as r:
+    view_songs = input("Would you like to view your songs?")
+    create_playlist = input("Would you like to create a playlist?")
+    add_songs = input("Would you like to add a new song to your song library?")
+    if add_songs == "yes" or "Yes":
+        add_songs()
+    else:
+        menu(login,register)
+
+    if view_songs == "yes" or "Yes":
+        view_songs()
+    else:
+        menu(login,register)
+    if create_playlists == "yes" or "Yes":
+        playlists(create,view)
+    else:
+        menu(login,register)
+    
+        
+def add_songs():
+        song_title = input("Please enter your new song title:")
+        artist_name = input("Please enter the artist's name:")
+        song_length = input("Please enter the song length:")
+        writefile = open("Song library.txt", "a")
+        writefile.write(song_title + '-' + artist_name + '-' + song_length + '\n')
+        writefile.close()
+
+def view_songs():
+    with open('Song library.txt', 'r') as r:
          for line in sorted(r):
             print( line , end='')
     
-                
+               
 def playlists(create,view):
-    
-    playlists = input("Would you like to create a playlist if so press 1 else press 2 to view other playlsits:")
+    playlists = input("To create a playlist press 1 else press 2 to view other playlsits or press 3 to return to main menu:")
     if playlists == "1":
         create()
-    else:
+    elif playlists == "2":
         view()
+    elif playlists == "3":
+        menu(login,register)
     
 
 def create():
     time_limit = (input("Please enter the time limit you would like the plsylist to last till:"))
     genre = input("Please enter the genre you would like your playlist to be:")
     artists_name = input("Please enter an artists name:")
-    writefile = open("Playlists.txt" , "a")
+    writefile = open("Playlists.txt" , "x")
     writefile.write('\n' + time_limit + ',' + genre + ',' + artists_name + '.')
     writefile.close()
 
@@ -59,7 +87,8 @@ def view():
             print( line , end='')
 
 
-menu(login,register)  
+menu(login,register)
 playlists(create,view)
+
 
 
